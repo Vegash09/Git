1ef8fba24c60bdcddb750d976b2bfd9724b3fd68
Vegash P <Vegash.P@diggibyte.com>
2025-06-19 15:17:01 +0530

    Just Addding some code in main.py files just to see the tag difference

--- Practice/Tagging/Creating_Tags_Practicing.py
+++ Practice/Tagging/Creating_Tags_Practicing.py
@@ -3,5 +3,8 @@ name = "Vegash"
 reversed_name = name[::-1]
 print("Reversed name is:", reversed_name)
 
+numbers = [2, 4, 6, 8]
+squared = [n**2 for n in numbers]
+print("Squared list:", squared)
 
 
